cdf9c84da7fadc6ce8d3191e1a244f51f09654f6
Muthamizh@wgs <muthamizh@whyglobalservices.com>
2024-06-14 03:03:38 +0530

    Split

--- src/utils/yield.split.js
+++ src/utils/yield.split.js
@@ -73,19 +73,63 @@ const SpliteYield = async (userId) => {
         findSlotToActivate.status = "Activated"
         findSlotToActivate.save()
         await Yield.findOneAndUpdate({slotId:findSlotToActivate},{status:"Activated"},{new:true})
-        // let findSlotById = await Slot.findById(element.slotId);
-        // console.log(findSlotById,"SLOT");
-        // if (findSlotById) {
-        //   let findThreeSlot = await Slot.countDocuments({ status: "Activated", createdAt: { $gt: findSlotById.createdAt } });
-        //   console.log(findThreeSlot,"LKLK");
-        //   let findSpacer = await Setting.findOne({_id:{$ne:null}})
-        //   if (findThreeSlot >= findSpacer.Sapcer) {
-        //     await Slot.findByIdAndUpdate(findSlotByUserId[0]._id, { status: "Activated" }, { new: true });
-        //     await Yield.findByIdAndUpdate(findSlotByUserId[0]._id, { status: "Activated" }, { new: true });
-        //   }
-        // }
+        // Split
+        let findExistingActivatedSlots = await Yield.find({status: "Activated", userId:{$ne:userId} });
+        console.log(findExistingActivatedSlots, "Existing Active Slots");
+  
+         let findLOY = await AdminYield.findOne().sort({ createdAt: -1 });
+          let findExistingActivatedSlotsCount = await Yield.countDocuments({status: "Activated",userId:{$ne:userId} });
+  
+          if (findExistingActivatedSlotsCount > 0 && findLOY) {
+            let splitAmount = (findLOY.Yield + 100) / findExistingActivatedSlotsCount;
+              findLOY.Yield = 0;
+              await findLOY.save();
+              const splitTwo = splitAmount / 2;
+              for (let element of findExistingActivatedSlots) {
+                console.log(element);
+          
+                let YIELD = element.currentYield + splitAmount;
+                console.log(YIELD, "Updated Yield");
+          
+                if (YIELD > 200) {
+                  let staticAmount = 100;
+                  let rem = YIELD - 200;
+                  findLOY.Yield = rem.toFixed(4);
+                  console.log(findLOY);
+                  await findLOY.save();
+          
+                  element.crowdStock = staticAmount.toFixed(4);
+                  element.currentYield = staticAmount.toFixed(4);
+                  element.status = "Completed";
+                  await element.save();
+                  console.log(element);
+          
+                  await Slot.findByIdAndUpdate(element.slotId, { status: "Completed" }, { new: true });
+                  await Yeild_history.create({ userId: element.userId, slotId: element.slotId, currentYield: splitAmount.toFixed(4) });
+                } else {
+                  element = await Yield.findByIdAndUpdate(
+                    element._id,
+                    {
+                      $inc: {
+                        wallet: parseFloat(splitTwo.toFixed(4)),
+                        crowdStock: parseFloat(splitTwo.toFixed(4)),
+                        currentYield: parseFloat(splitAmount.toFixed(4)),
+                      }
+                    },
+                    { new: true }
+                  );
+                  console.log(element,"element");
+                  await Yeild_history.create({ userId: element.userId, slotId: element.slotId, currentYield: splitAmount.toFixed(4) });
+                }
+                if (element.currentYield >= element.totalYield) {
+                  element.status = "Completed";
+                  await element.save();
+                  await Slot.findByIdAndUpdate(element.slotId, { status: "Completed" }, { new: true });
+                }
+              }
+          }
+        }
       }
-    }
   }
 };
 
